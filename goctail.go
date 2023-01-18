1db33d5bb07a2ddf219ad8cd8b7205d9a0d8f164
bluszcz <bluszcz@bluszcz.net>
2023-01-18 10:07:52 +0100

    flag

--- goctail.go
+++ goctail.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var flagvar int
 
 func main() {
+	flag.IntVar(&flagvar, "flagname", 1234, "help message for flagname")
+	flag.Parse()
+	// fmt.Println("ip has value ", *ip)
+	fmt.Println("flagvar has value ", flagvar)
 	fmt.Println("Hello, World!")
 }
