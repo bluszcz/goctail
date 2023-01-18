825c4eb3d4d54fd3ab5618f7ac0445bdf9a0b0d8
Rafal Zawadzki <bluszcz@bluszcz.net>
2023-01-18 16:27:47 +0100

    Added a bit of README and first parsing command line

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # goctail
 Colorful configurable log output
+
+## Other tail implementations
+
+* [coreutils tail - c](https://github.com/coreutils/coreutils/blob/master/src/tail.c)
+* [multitail - c](https://github.com/halturin/multitail)
+
+## Colorizers
+
+* [grc](https://github.com/garabik/grc  )
--- goctail.go
+++ goctail.go
@@ -3,14 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 var flagvar int
+var lastlinesvar int
+var boolvar bool
+var debug string
+
+func LogPrintln(line string) {
+	if debug == "true" {
+		log.Println(line)
+	}
+}
+
+func ReturnLastLines(lines int, filename string) {
+	file, err := os.Open(filename) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	stats, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var filesize = stats.Size()
+	fmt.Printf("The file is %d bytes long", filesize)
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+}
 
 func main() {
+	var debug string
+
+	flag.IntVar(&lastlinesvar, "n", 10, "last lines to show")
 	flag.IntVar(&flagvar, "flagname", 1234, "help message for flagname")
+	flag.BoolVar(&boolvar, "f", false, "help message for flagname")
+
 	flag.Parse()
-	// fmt.Println("ip has value ", *ip)
-	fmt.Println("flagvar has value ", flagvar)
-	fmt.Println("Hello, World!")
+	files := flag.Args()
+	LogPrintln("files" + strings.Join(files, "-"))
+
+	if debug == "true" {
+		fmt.Println("flagvar has value ", flagvar)
+		fmt.Println("last lines ", lastlinesvar)
+		fmt.Println("boolvar ", boolvar)
+		fmt.Println("Hello, World!")
+		fmt.Println(" DEBUG <<<<")
+		fmt.Println("")
+
+	}
+
+	ReturnLastLines(lastlinesvar, files[0])
 }
