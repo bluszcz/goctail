d593c80d83a61f7bff8897b097001c6e1062a6e7
bluszcz <bluszcz@bluszcz.net>
2023-01-20 22:30:32 +0100

    Takes more line and adds buffer - seeking problem fixed

--- goctail.go
+++ goctail.go
@@ -51,67 +51,106 @@ func ReturnLastCount(count int, filename string) {
 
 // Performed reversed read in BUFSIZ chunks
 func readReverseChunk(start int, filesize int, file *os.File) []byte {
-	LogPrintln("readReverseChunk")
+	LogPrintln("readReverseChunk", fmt.Sprint(start))
 
 	// file.Seek(512, )
 	data := make([]byte, BUFSIZ)
 
-	var offsetType int
-	if start == filesize {
-		offsetType = os.SEEK_END
-	} else {
-		offsetType = os.SEEK_CUR
-	}
+	// var offsetType int
+	// if start == filesize {
+	// 	offsetType = os.SEEK_END
+	// } else {
+	// 	LogPrintln(fmt.Sprint("Current"))
+	// 	offsetType = os.SEEK_CUR
+	// }
 
-	file.Seek(int64(BUFSIZ)*-1, offsetType)
+	// file.Seek(i6t64(BUFSIZ)*-1, offsetType)
+	file.Seek(int64(start)-int64(BUFSIZ), os.SEEK_SET)
 	count, err := file.Read(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Printf("%s\n", data[:count])
+	fmt.Printf("%s\n", data[:count])
 
 	return data[:count]
+}
+
+func processChunkedData(result string, lines int, start int, filesize int, file *os.File) {
+	var amountEndlines = strings.Count(result, "\n")
+	LogPrintln("How many endlines", fmt.Sprint(amountEndlines), fmt.Sprint(lines))
+	if amountEndlines == lines {
+		fmt.Printf("%s\n", result)
+	} else if amountEndlines > lines+1 {
+		for amountEndlines > lines {
+			LogPrintln(fmt.Sprint("a"))
+			amountEndlines = strings.Count(result, "\n")
+			LogPrintln("How many endlines2", fmt.Sprint(amountEndlines), fmt.Sprint(lines))
+
+			var indexN = strings.Index(result, "\n")
+			amountEndlines = strings.Count(result, "\n")
+			LogPrintln("How many endlines3", fmt.Sprint(amountEndlines), fmt.Sprint(lines))
+
+			LogPrintln("indexX", fmt.Sprint(indexN))
+			// if indexN > 0 {
+			result = result[indexN+1:]
+			// } else {
+			// result = result[1:]
+			// }
+			LogPrintln(fmt.Sprint(amountEndlines))
+
+			LogPrintln("amountEndLines", fmt.Sprint(amountEndlines))
+		}
+		fmt.Printf("%s", result)
+	} else {
+		LogPrintln(fmt.Sprint("Gotta do else"))
+		var newResult string
+		start = start - BUFSIZ
+		newResult = fmt.Sprint(readReverseChunk(start, filesize, file))
+		newResult = newResult + result
+		processChunkedData(result, lines, start, filesize, file)
+	}
 }
 
 func ReturnLastLines(lines int, filename string) {
 	file, filesize := GetFile(filename)
 	LogPrintln("filesize: " + fmt.Sprint(filesize))
 	var result string = fmt.Sprintf("%s", readReverseChunk(int(filesize), int(filesize), file))
-	var amountEndlines = strings.Count(result, "\n")
-	LogPrintln("How many endlines", fmt.Sprint(amountEndlines))
-	// data := make([]byte, 100)
-	// count, err := file.Read(data)
-	// if err != nil {
-	// 	log.Fatal(err)
+	processChunkedData(result, lines, int(filesize), int(filesize), file)
+	// var amountEndlines = strings.Count(result, "\n")
+	// LogPrintln("How many endlines", fmt.Sprint(amountEndlines))
+	// // data := make([]byte, 100)
+	// // count, err := file.Read(data)
+	// // if err != nil {
+	// // 	log.Fatal(err)
+	// // }
+
+	// // var indexN = strings.Index(result, "\n")
+	// if amountEndlines == lines {
+	// 	fmt.Printf("%s\n", result)
+	// } else if amountEndlines > lines+1 {
+
+	// 	for amountEndlines > lines {
+	// 		LogPrintln(fmt.Sprint("a"))
+	// 		var indexN = strings.Index(result, "\n")
+
+	// 		LogPrintln("indexX", fmt.Sprint(indexN))
+	// 		if indexN > 0 {
+	// 			result = result[indexN:]
+	// 		} else {
+	// 			result = result[1:]
+	// 		}
+	// 		// fmt.Printf(fmt.Sprint(strings.Index(result, "\n")))
+
+	// 		// indexN = strings.Index(result, "\n")
+	// 		LogPrintln(fmt.Sprint(amountEndlines))
+
+	// 		amountEndlines = strings.Count(result, "\n")
+	// 		LogPrintln("amountEndLines", fmt.Sprint(amountEndlines))
+	// 	}
+
+	// 	fmt.Printf("%s", result)
+
 	// }
-
-	// var indexN = strings.Index(result, "\n")
-	if amountEndlines == lines {
-		fmt.Printf("%s\n", result)
-	} else if amountEndlines > lines+1 {
-
-		for amountEndlines > lines {
-			LogPrintln(fmt.Sprint("a"))
-			var indexN = strings.Index(result, "\n")
-
-			LogPrintln("indexX", fmt.Sprint(indexN))
-			if indexN > 0 {
-				result = result[indexN:]
-			} else {
-				result = result[1:]
-			}
-			// fmt.Printf(fmt.Sprint(strings.Index(result, "\n")))
-
-			// indexN = strings.Index(result, "\n")
-			LogPrintln(fmt.Sprint(amountEndlines))
-
-			amountEndlines = strings.Count(result, "\n")
-			LogPrintln("amountEndLines", fmt.Sprint(amountEndlines))
-		}
-
-		fmt.Printf("%s", result)
-
-	}
 
 }
 
