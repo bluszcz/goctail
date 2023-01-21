598913d1c242acabe651c71396b3e4205649dd92
Rafał Zawadzki <bluszcz@bluszcz.net>
2023-01-21 09:53:38 +0100

    Code cleanup

--- goctail.go
+++ goctail.go
@@ -32,7 +32,6 @@ func GetFile(filename string) (*os.File, int64) {
 		log.Fatal(err)
 	}
 	var filesize = stats.Size()
-	// fmt.Printf("The file is %d bytes long", filesize)
 	return file, filesize
 }
 
@@ -51,52 +50,29 @@ func ReturnLastCount(count int, filename string) {
 
 // Performed reversed read in BUFSIZ chunks
 func readReverseChunk(start int, filesize int, file *os.File) string {
-	// fmt.Printf("start %d", start)
 	LogPrintln("readReverseChunk", fmt.Sprint(start))
-	// file.Seek(512, )
-	// var offsetType int
-	// if start == filesize {
-	// 	offsetType = os.SEEK_END
-	// } else {
-	// 	LogPrintln(fmt.Sprint("Current"))
-	// 	offsetType = os.SEEK_CUR
-	// }
-	// fmt.Printf("%d\n" ,start-BUFSIZ)
-
+	
 	var tmpBufSiz int
 	if start>=BUFSIZ {
 		tmpBufSiz = BUFSIZ
 		file.Seek(int64(start)-int64(BUFSIZ), os.SEEK_SET)
 
 	} else {
-		// tmpBufSiz = 237
 		tmpBufSiz = start
 		start = 0
-		// tmpBufSiz = BUFSIZ-start
-		// fmt.Printf("%d %d %d %d\n", BUFSIZ, start, start-BUFSIZ, tmpBufSiz)
 		file.Seek(0, 0)
-
 	}
-	// fmt.Printf(">> BUFSIZ tmp %d  <<<\n", tmpBufSiz)
 	data := make([]byte, tmpBufSiz)
-	// file.Seek(i6t64(BUFSIZ)*-1, offsetType)
 	count, err := file.Read(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Printf("%s\n", data[:count])
-
 	return fmt.Sprintf("%s", data[:count])
 }
 
 func processChunkedData(result string, lines int, start int, filesize int, file *os.File) {
 	var amountEndlines = strings.Count(result, "\n")
-	// fmt.Printf(">>>>>>> %d %d %s <<<< \n", amountEndlines, lines, result)
 	LogPrintln("How many endlines", fmt.Sprint(amountEndlines), fmt.Sprint(lines))
-	// if amountEndlines == lines {
-	// 	fmt.Printf(">>>>>>> EQUAL")
-	// 	fmt.Printf("%s", result)
-	// } else 
 	if amountEndlines > lines {
 		var indexN = strings.Index(result, "\n")
 		result = result[indexN+1:]
@@ -108,13 +84,8 @@ func processChunkedData(result string, lines int, start int, filesize int, file 
 			amountEndlines = strings.Count(result, "\n")
 			LogPrintln("How many endlines3", fmt.Sprint(amountEndlines), fmt.Sprint(lines))
 			LogPrintln("indexX", fmt.Sprint(indexN))
-			// if indexN > 0 {
 			result = result[indexN+1:]
-			// } else {
-			// result = result[1:]
-			// }
 			LogPrintln(fmt.Sprint(amountEndlines))
-
 			LogPrintln("amountEndLines", fmt.Sprint(amountEndlines))
 		}
 		fmt.Printf("%s", result)
@@ -140,42 +111,6 @@ func ReturnLastLines(lines int, filename string) {
 	LogPrintln("filesize: " + fmt.Sprint(filesize))
 	var result string = fmt.Sprintf("%s", readReverseChunk(int(filesize), int(filesize), file))
 	processChunkedData(result, lines, int(filesize), int(filesize), file)
-	// var amountEndlines = strings.Count(result, "\n")
-	// LogPrintln("How many endlines", fmt.Sprint(amountEndlines))
-	// // data := make([]byte, 100)
-	// // count, err := file.Read(data)
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
-
-	// // var indexN = strings.Index(result, "\n")
-	// if amountEndlines == lines {
-	// 	fmt.Printf("%s\n", result)
-	// } else if amountEndlines > lines+1 {
-
-	// 	for amountEndlines > lines {
-	// 		LogPrintln(fmt.Sprint("a"))
-	// 		var indexN = strings.Index(result, "\n")
-
-	// 		LogPrintln("indexX", fmt.Sprint(indexN))
-	// 		if indexN > 0 {
-	// 			result = result[indexN:]
-	// 		} else {
-	// 			result = result[1:]
-	// 		}
-	// 		// fmt.Printf(fmt.Sprint(strings.Index(result, "\n")))
-
-	// 		// indexN = strings.Index(result, "\n")
-	// 		LogPrintln(fmt.Sprint(amountEndlines))
-
-	// 		amountEndlines = strings.Count(result, "\n")
-	// 		LogPrintln("amountEndLines", fmt.Sprint(amountEndlines))
-	// 	}
-
-	// 	fmt.Printf("%s", result)
-
-	// }
-
 }
 
 // https://stackoverflow.com/questions/35809252/check-if-flag-was-provided-in-go
@@ -193,10 +128,8 @@ func isFlagPassed(name string) bool {
 func main() {
 	debug = os.Getenv("DEBUG")
 	// Need to set priority of parsing flags
-
 	flag.IntVar(&lastlinesvar, "n", 10, "last lines to show")
 	flag.IntVar(&lastbytestvar, "c", 100, "last bytes to show")
-	flag.IntVar(&flagvar, "flagname", 1234, "help message for flagname")
 	flag.BoolVar(&boolvar, "f", false, "help message for flagname")
 
 	flag.Parse()
@@ -204,7 +137,6 @@ func main() {
 	LogPrintln(fmt.Sprintf("n %t", isFlagPassed("n")))
 	files := flag.Args()
 	LogPrintln("files" + strings.Join(files, "-"))
-	LogPrintln("flagvar has value ", fmt.Sprint(flagvar))
 	LogPrintln("last lines ", fmt.Sprint(lastlinesvar))
 	LogPrintln("boolvar ", fmt.Sprint(boolvar))
 	LogPrintln("Hello, World!")
